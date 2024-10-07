51cde458f4cac94196613e314cfd8bdad07f184e
MinhSang97 <minhsangnguyen463@gmail.com>
2024-10-07 17:08:07 +0700

    test tls

--- example/1.6/cs/central_system_sim.go
+++ example/1.6/cs/central_system_sim.go
@@ -27,8 +27,8 @@ const (
 	envVarServerPort           = "SERVER_LISTEN_PORT"
 	envVarTls                  = "true"
 	envVarCaCertificate        = "CA_CERTIFICATE_PATH"
-	envVarServerCertificate    = "cert.pem"
-	envVarServerCertificateKey = "privkey.pem"
+	envVarServerCertificate    = "/cert.pem"
+	envVarServerCertificateKey = "/privkey.pem"
 )
 
 var log *logrus.Logger
